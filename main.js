989e5ea315276d83a85f1e4391f75230e742dde0
streng1x <boom.kb09@email.com>
2025-09-24 18:32:33 +0600

    Добавил JS - Азим

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// 
+    function toggleDropdown() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // закрытие при клике вне меню
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
